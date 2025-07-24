6c6984a04f28b54e69ff29bcf7d064da214387ea
farrrahhh <18222096@std.stei.itb.ac.id>
2025-07-24 20:28:21 +0700

    feat: transaction and product database

--- backend/models/tables.py
+++ backend/models/tables.py
@@ -52,3 +52,30 @@ class Message(Base):
     timestamp = Column(TIMESTAMP, server_default=func.now(), nullable=False)
 
     conversation = relationship("Conversation", back_populates="messages")
+
+# Product Table
+class Product(Base):
+    __tablename__ = "products"
+
+    product_id = Column(String(50), primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+    description = Column(Text, nullable=True)
+    video_url = Column(String(255), nullable=True)
+    photo_url = Column(String(255), nullable=True)
+
+    transactions = relationship("Transaction", back_populates="product", cascade="all, delete-orphan")
+
+
+# Transaction Table
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    transaction_id = Column(String(50), primary_key=True, index=True)
+    user_id = Column(String(8), ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    product_id = Column(String(50), ForeignKey("products.product_id", ondelete="CASCADE"), nullable=False)
+    status = Column(String(50), nullable=False)
+
+    user = relationship("User")
+    product = relationship("Product", back_populates="transactions")
